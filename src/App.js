c45c3273fc6dbd0e3a517d7d8884431a160c47e8
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-08-27 13:23:52 -0500

    set up

---
+++ src/App.js
@@ -0,0 +1,6 @@
+//import react
+import React from 'react';
+
+const App = () => <h1>Set up</h1>;
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,18 @@
+//import react, react dom and react router dom
+import React from 'react';
+import ReactDom from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+//import app
+import App from './App'
+
+//const for get the app
+const root = document.getElementById('app');
+
+//render react dom
+ReactDom.render(
+	<Router>
+		<App />
+	</Router>,
+	root,
+);
