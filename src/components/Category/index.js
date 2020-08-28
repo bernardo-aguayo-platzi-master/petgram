3761cac5a2191a1f260301198ac3bf057aeae3df
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-08-28 10:53:32 -0500

    fetch data from dog api and set the values

--- src/components/Category/index.js
+++ src/components/Category/index.js
@@ -4,15 +4,11 @@ import React from 'react';
 //import styled components
 import { Anchor, Image } from './styles';
 
-//const default image
-const defaultImage =
-	'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRq6A3dpFslGtcwfut1iFd0o7kr_uFfVhjdhQ&amp;usqp=CAU';
-
-export const Category = ({ cover = defaultImage, path, emoji = '?' }) => (
+export const Category = ({ cover }, path) => (
   <>
+    {console.log(cover)}
     <Anchor href={path}>
       <Image src={cover} />
-      {emoji}
     </Anchor>
   </>
 );
--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,16 +1,30 @@
 //import react
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 //import category component
 import { Category } from '../Category';
 //use styled components
 import { List, Item } from './styles';
 
-export const ListOfCategories = () => (
-  <List>
-    {[1, 2, 3, 4].map((element) => (
-      <Item key={element}>
-        <Category />
-      </Item>
-    ))}
-  </List>
-);
+export const ListOfCategories = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(()=>{
+    const getPets = () => {
+      fetch('https://dog.ceo/api/breeds/image/random/5')
+        .then(response => response.json())
+        .then(data => setCategories(data.message));
+    }
+    getPets();
+
+  }, [])
+
+  return (
+    <List>
+      {categories.map((pet, i) => (
+        <Item key={i}>
+          <Category cover={pet}/>
+        </Item>
+      ))}
+    </List>
+  );
+};
