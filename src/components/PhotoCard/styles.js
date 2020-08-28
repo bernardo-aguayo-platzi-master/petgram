31500a2c8cd762554deaf66db861985b0cafc103
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-08-28 13:23:01 -0500

    some minor bugs with card components

--- src/components/PhotoCard/styles.js
+++ src/components/PhotoCard/styles.js
@@ -1,28 +1,28 @@
 //import styled components
 import styled from 'styled-components';
 
-//img wrapper
-export const ImgWrapper = styled.figure`
-	border-radius: 10px;
-	display: block;
-	height: 0;
-	overflow: hidden;
-	padding: 56.25% 0 0 0;
-	position: relative;
-	width: 100%;
-`;
+//image wrapper
+export const ImgWrapper = styled.div`
+  border-radius: 10px;
+  display: block;
+  height: 0;
+  overflow: hidden;
+  padding: 56.25% 0 0 0;
+  position: relative;
+  width: 100%; 
+`
 
-//img component
+//image
 export const Image = styled.img`
-	box-shadow: 0 10px 14px rgba(0, 0, 0, 0.2);
-	height: 100%;
-	object-fit: cover;
-	position: absolute;
-	top: 0;
-	width: 100%;
-`;
+  box-shadow: 0 10px 14px rgba(0, 0, 0, .2);
+  height: 100%;
+  object-fit: cover;
+  position: absolute;
+  top: 0;
+  width: 100%;
+`
 
-//button component
+//button
 export const Button = styled.button`
   display: flex;
   align-items: center;
@@ -30,4 +30,4 @@ export const Button = styled.button`
   & svg {
     margin-right: 4px;
   }
-`;
+`
