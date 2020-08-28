964ab2debd3128c3e47a0106e10cdb8bd96adcfe
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-08-27 21:07:01 -0500

    use styled component with categoy component

--- src/components/category/index.js
+++ src/components/category/index.js
@@ -1,17 +1,18 @@
 //import react
 import React from 'react';
 
-//const default image
-const defaultImage = 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRq6A3dpFslGtcwfut1iFd0o7kr_uFfVhjdhQ&amp;usqp=CAU';
+//import styled components
+import { Anchor, Image } from './styles';
 
-export const Category = (
-  { cover = defaultImage,
-    path,
-    emoji = '?' }) => (
+//const default image
+const defaultImage =
+	'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRq6A3dpFslGtcwfut1iFd0o7kr_uFfVhjdhQ&amp;usqp=CAU';
+
+export const Category = ({ cover = defaultImage, path, emoji = '?' }) => (
   <>
-    <a href={path}>
-      <img src={cover}/>
+    <Anchor href={path}>
+      <Image src={cover} />
       {emoji}
-    </a>
+    </Anchor>
   </>
-)
+);
